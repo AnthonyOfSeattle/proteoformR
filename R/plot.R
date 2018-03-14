8fec6b384f13dc41c66598c02f5ec696dd7c5d7b
AnthonyOfSeattle <valenta4@uw.edu>
2018-03-14 09:48:25 -0700

    Initialized plotting framework

---
+++ R/plot.R
@@ -0,0 +1,11 @@
+plot.bpmodel.single <- function(model, ref = NULL, dir = NULL){
+  if (is.null(ref)){
+    plot_ref = unique(model$Input$ref)[1]
+  } else {
+    plot_ref = ref
+  }
+  
+  tidy_input = model$Input %>%
+    gather("batch", "vals", 4:6)
+  print(to_plot)
+}
