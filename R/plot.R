5344ba76e3ddaa504c809822494cc7725f13a058
AnthonyOfSeattle <valenta4@uw.edu>
2018-03-14 16:41:40 -0700

    Added some plotting

--- R/plot.R
+++ R/plot.R
@@ -6,6 +6,20 @@ plot.bpmodel.single <- function(model, ref = NULL, dir = NULL){
   }
   
   tidy_input = model$Input %>%
+    filter(ref == plot_ref) %>%
+    mutate(peptide = as.factor(1:dim(.)[1])) %>%
     gather("batch", "vals", 4:6)
-  print(to_plot)
+
+  cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+  plt = ggplot(tidy_input, aes(x = start,
+                               xend = end,
+                               y = vals,
+                               yend = vals,
+                               color = batch))
+  plt = plt + geom_segment(lwd = 2)
+  plt = plt + ggtitle(plot_ref) + theme_bw() + scale_colour_manual(values=cbPalette)
+  
+  if (is.null(dir)){
+    print(plt)
+  }
 }
