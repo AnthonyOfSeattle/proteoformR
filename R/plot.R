3f1f625098eae7270bba6862621a076de86d9ea3
AnthonyOfSeattle <valenta4@uw.edu>
2018-05-09 16:42:55 -0700

    Consolidated plotting

--- R/plot.R
+++ R/plot.R
@@ -1,65 +1,3 @@
-#' View a single breakpoint model
-#' 
-#' \code{plot.bpmodel.single} builds single ggplot2 breakpoint figures for criticism. Can be called specifically,
-#' but was originally intended to be called from plot.bpmodel. The default behavior, by leaving
-#' ref and dir null, is to plot the first reference and print to stdout. Uses a color safe pallate
-#' for now, which can plot up to 8 samples.
-#' 
-#' @param model S3 object supplied by running proteoformR
-#' @param ref The protein reference to extract and plot
-#' @param dir Directory to save plot if supplied, but NULL specifies print
-#' @seealso \code{\link{proteoformR}} and \code{\link{plot.bpmodel}}
-
-plot.bpmodel.single <- function(model, ref = NULL, dir = NULL){
-  if (is.null(ref)){
-    plot_ref = unique(model$Input$ref)[1]
-  } else {
-    plot_ref = ref
-  }
-  
-  tidy_input = model$Input %>%
-    filter(ref == plot_ref) %>%
-    gather("batch", "vals", 4:dim(.)[2])
-
-  cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
-  plt = ggplot()
-  
-  # Add model first so it is behind
-  filtered_model = model$ModelFit %>% 
-    filter(ref == plot_ref) %>%
-    mutate(pos = (start+end)/2 )
-  plt = plt + geom_line(data = filtered_model,
-                        aes( x = pos,
-                             y = fit),
-                        lwd = 2)
-
-  # Add points on top
-  plt = plt + geom_segment(data=tidy_input, aes(x = start,
-                                           xend = end,
-                                           y = vals,
-                                           yend = vals,
-                                           color = batch),
-                           lwd = 2)
-  
-  plt = plt + ggtitle(plot_ref) + theme_bw() + scale_colour_manual("Sample",
-                                                                   values=cbPalette)
-  plt = plt + xlab("Position") + ylab("Z-score")
-  plt = plt + theme(axis.text=element_text(size=14),
-                    axis.title=element_text(size=16,face="bold"),
-                    legend.text=element_text(size = 14),
-                    legend.title=element_text(size = 16))
-  
-  if (is.null(dir)){
-    print(plt)
-  } else {
-    ggsave(filename= paste(dir, "/",
-                           plot_ref, ".png",
-                           sep = ""),
-           plot = plt)
-  }
-}
-
-
 #' View all or a subset of breakpoint models
 #' 
 #' \code{plot.bpmodel} builds ggplot2 breakpoint figures for criticism. 
@@ -74,15 +12,69 @@ plot.bpmodel.single <- function(model, ref = NULL, dir = NULL){
 #' 
 #' @seealso \code{\link{proteoformR}}
 
-plot.bpmodel <- function(model, ref = "All", dir = getwd(), only.calls = TRUE){
-  if (ref == "All") {
-    if(only.calls){
-      ref = unique(model$BreakPoints$ref)
-    } else { 
-        ref = unique(model$Input$ref)
-        }
+plot.bpmodel <- function(model, ref = NULL, dir = NULL, only.calls = TRUE){
+  if (only.calls){
+    ref_list = unique(model$BreakPoints$ref)
+  } else { 
+    ref_list = unique(model$Input$ref)
   }
-  for (r in ref){
-    plot.bpmodel.single(model, ref = r, dir = dir)
+  if (is.null(ref)){
+    ref_list = ref_list[1]
+  } else if (ref == "All"){
+    ref_list = ref_list
+  } else{
+    ref_list = ref
   }
+  for (r in ref_list){
+    plt = .build.single.plot(model, plot_ref = r, dir = dir)
+    if (is.null(dir)){
+      print(plt)
+    } else {
+      ggsave(filename= paste(dir, "/",
+                             r, ".png",
+                             sep = ""), dplot = plt)
+    }
+  }
+}
+
+# Global color blind pallate for plotting
+cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+
+# Create a single plot and determine where to send it
+.build.single.plot <- function(model, plot_ref, dir){
+  
+  plt = ggplot()
+  
+  # Add model first so it is behind then add points on top
+  plt = plt + 
+    geom_line(data = model$ModelFit %>% 
+                filter(ref == plot_ref) %>%
+                mutate(pos = (start+end)/2 ),
+              aes( x = pos,
+                   y = fit),
+              lwd = 2) +
+    geom_segment(data= model$Input %>%
+                   filter(ref == plot_ref) %>%
+                   gather("batch", "vals", 4:dim(.)[2]),
+                 aes(x = start,
+                     xend = end,
+                     y = vals,
+                     yend = vals,
+                     color = batch),
+                 lwd = 2)
+  
+  # Add default features
+  plt = plt + 
+    ggtitle(plot_ref) + 
+    xlab("Position") +
+    ylab("Z-score") +
+    theme_bw() + 
+    scale_colour_manual("Sample",
+                        values=cbPalette) +
+    theme(axis.text=element_text(size=14),
+          axis.title=element_text(size=16,face="bold"),
+          legend.text=element_text(size = 14),
+          legend.title=element_text(size = 16))
+          
+  return(plt)
 }
