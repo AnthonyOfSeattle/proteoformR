ee531d9ef04443a514f4fa3f4c085d0803774545
AnthonyOfSeattle <valenta4@uw.edu>
2018-05-10 14:01:57 -0700

    Refactor: single class for loss calculation

---
+++ src/loss_calculator.cpp
@@ -0,0 +1,38 @@
+#include <Rcpp.h>
+#include "utils.h"
+#include "loss_calculator.h"
+using namespace Rcpp;
+
+/* Initialize each vector in the loss calculator
+ * to hold each batch sepperate, which will allow
+ * independent calculation of the batch means
+ */
+LossCalculator::LossCalculator(int nsamples){
+  NumericVector per_sample_n_ = rep(0., nsamples);
+  NumericVector moment_one_ = rep(0., nsamples);
+  NumericVector moment_two_ = rep(0., nsamples);
+}
+
+
+/* Calculating the minimum variance unbiased estimator
+ * can be done entirely using E[X] and E[X**2].
+ * This means that we can keep track of additive
+ * sufficient statistics and only evaluate the loss when
+ * it is needed.
+ */
+void LossCalculator::Update(NumericVector values){
+  per_sample_n_ = ifelse(!(is_na(values)), per_sample_n_ + 1., per_sample_n_);
+  moment_one_ = ifelse(!(is_na(values)), moment_one_ + values, moment_one_);
+  moment_two_ = ifelse(!(is_na(values)), moment_two_ +  pow(values , 2), moment_two_); 
+}
+
+
+/* Transform saved additive sufficient statistics into a squared loss.
+ * Ignore batch slots that are absent of updates.
+ */
+double LossCalculator::GetLoss(){
+  NumericVector current_error = ifelse(!(per_sample_n_ == 0),
+                                       moment_two_ - pow(moment_one_, 2)/per_sample_n_,
+                                       0.);
+  return(sum(current_error));
+}
---
+++ src/loss_calculator.h
@@ -0,0 +1,30 @@
+#ifndef LOSS_CALCULATOR_H
+#define LOSS_CALCULATOR_H
+
+#include <Rcpp.h>
+using namespace Rcpp;
+
+/* Container to store and calculate loss over
+ * a section of sequntial data data points.
+ * Utilizes lazy evaluation of the loss by
+ * storing additive sufficient statistics with
+ * each update. Transforms statisitics into the 
+ * squared loss against the mean with a call to GetLoss
+ * 
+ * Example:
+ *     LossCalculator c = LossCalulator(3);
+ *     c.Update(NumericVector::create(1.0, 2.0, 3.0));
+ *     c.Update(NumericVector::create(2.0, 3.0, 4.0));
+ *     double error = c.GetLoss;
+ */
+class LossCalculator{
+  NumericVector per_sample_n_;
+  NumericVector moment_one_;
+  NumericVector moment_two_;
+public:
+  LossCalculator(int);
+  void Update(NumericVector);
+  double GetLoss();
+};
+
+#endif
