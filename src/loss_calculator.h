3fce08351ee4d4ca40e856a6a4cb7697fcf4af97
AnthonyOfSeattle <valenta4@uw.edu>
2018-05-11 13:09:39 -0700

    Updating comment

--- src/loss_calculator.h
+++ src/loss_calculator.h
@@ -10,12 +10,6 @@ using namespace Rcpp;
  * storing additive sufficient statistics with
  * each update. Transforms statisitics into the 
  * squared loss against the mean with a call to GetLoss
- * 
- * Example:
- *     LossCalculator c = LossCalulator(3);
- *     c.Update(NumericVector::create(1.0, 2.0, 3.0));
- *     c.Update(NumericVector::create(2.0, 3.0, 4.0));
- *     double error = c.GetLoss;
  */
 class LossCalculator{
   int nsamp;
