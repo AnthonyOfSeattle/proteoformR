15b90530270334bd6d472ad5c16fdf52f131735e
AnthonyOfSeattle <valenta4@uw.edu>
2018-05-15 21:58:32 -0700

    Gained all the functionality of the underlying LossCalculator. Originally this class wasn't providing enough new functionality to exist on its own, so I built it to calculate the full objective. Now it doens't need a bunch of forwarding methods.

--- src/objective_calculator.h
+++ src/objective_calculator.h
@@ -2,16 +2,26 @@
 #define OBJECTIVE_CALCULATOR_H
 
 #include <Rcpp.h>
-#include "loss_calculator.h"
 using namespace Rcpp;
 
+/* Container to store and calculate objective over
+ * a section of sequential data points.
+ * Utilizes lazy evaluation of the loss by
+ * storing additive sufficient statistics with
+ * each update. Transforms statisitics into the penalized
+ * squared loss against the mean with a call to GetObjective.
+ */
 class ObjectiveCalculator{
+  int nsamp;
+  IntegerVector per_sample_n_;
+  NumericVector moment_one_;
+  NumericVector moment_two_;
   double objective_;
-  LossCalculator* loss_;
   double penalty_;
 public:
   ObjectiveCalculator(int, double);
-  ~ObjectiveCalculator();
+  ObjectiveCalculator(const ObjectiveCalculator&);
+  void ZeroAccumulators();
   void SetObjective(double);
   void Update(NumericMatrix::Row);
   double GetObjective();
